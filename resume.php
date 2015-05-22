3673a91436195643d08e12a666d8bb7954b31af3
ediniz <ediniz92@gmail.com>
2015-05-22 08:07:30 -0400

    Starting resume
    Starting resume

--- resume.php
+++ resume.php
@@ -1,5 +1,36 @@
 <?php include 'head.php';?>
 <?php include 'header.php';?>
+<div class="container">
+	<div class="row">
+		<h4 class="resumeheads">Summary of Qualifications</h4>
+		<ul class="resume">
+			<li>Motivated professional with broad technical skill set</li>
+			<li>Independently driven team player who can work well under pressure and tight deadlines</li>
+			<li>Great communicator with pleasant demeanour and outstanding client facing skills</li>
+			<li>Strong project management and organizational skills</li>
+			<li>Known for ability to multi-task multiple projects and meet deadlines</li>
+			<li>Excellent research skills, critical thinking and decision making ability with strong attention to details</li>
+			<li>Quick learner with interest in new and emerging technologies</li>
+		</ul>
+		<h4 class="resumeheads">Technical Skills</h4>
+		<ul class="resume">
+			<li>Hands-on experience with UI design, development and testing</li>
+			<li>Proficient in creating visual mockups and graphic pre-designs</li>
+			<li>Solid knowledge of HTML5, CSS3, jQuery, JavaScript, Bootstrap, WP and illustration/video/animation for web</li>
+			<li>High level understanding of web applications and features (multi-tier web apps, databases, cookies/session, HTTP get/post, RESTful API calls, web sessions)</li>
+			<li>Experience using version control software (Subversion, Git)</li>
+			<li>Proficient in Adobe CS-current and MS Office Suite</li>
+			<li>Experience in SaaS based software implementation and integration and strong CMS experience</li>
+			<li>Concrete knowledge of web design best practices, usability and accessibility standards, image optimization and responsive design</li>
+			<li>High level of proficiency in typography, branding and identity design</li>
+			<li>Strong analytical and diagnostic skills with ability to quickly identify and solve software issues</li>
+		</ul>
+		<h4 class="resumeheads">Education</h4>
+		<ul class="resume">
+			<li>Bachelor of Science, Information Systems Technology, Seminole State College (ongoing)</li>
+		</ul>
+	</div>
+</div>
 <script>
 $(document).ready(function() {
 	$('#resume').addClass('active');
