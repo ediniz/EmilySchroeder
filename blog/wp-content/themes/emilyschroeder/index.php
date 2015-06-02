67f060d3d7d2604e0aa0fdc2eef7540ca04fe017
ediniz <ediniz92@gmail.com>
2015-06-02 16:32:27 -0400

    Working through the loop

--- blog/wp-content/themes/emilyschroeder/index.php
+++ blog/wp-content/themes/emilyschroeder/index.php
@@ -3,9 +3,17 @@
 	<div class="row">
 		<div class="col-lg-8">
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
+				<?php 
+				if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+					the_post_thumbnail();
+				} 
+				?>
 				<h1><?php the_title() ;?></h1>
-				<?php the_post_thumbnail(); ?>
+				<p class="byline">
+					<div><?php the_time(__('F j, Y')); ?></div>
+					<div><?php the_author_posts_link(); ?></div>
+					<?php edit_post_link(__('Edit', 'example'), ' <span class="sep">|</span> <span class="edit">', '</span> '); ?>
+				</p>
 				<?php the_excerpt(); ?>
 
 			<?php endwhile; else: ?>
